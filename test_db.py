e1ac14984fbecf8249b07556bb607de63675bb10
Guillermo M Wechsler <guillermowechsler@Apu-Wamani.local>
2025-12-30 20:05:59 -0800

    Reorganize: move loaders to loaders/, remove utility scripts

--- test_db.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-conn = psycopg2.connect('postgresql://postgres.jtyykeaeupxbbkaqkfqp:CodeNess6504@aws-0-us-west-2.pooler.supabase.com:6543/postgres')
-cur = conn.cursor()
-cur.execute('SELECT count(*) FROM disasters')
-print('Connection successful! Disasters count:', cur.fetchone()[0])
-conn.close()
